2d90e137b2a6ca1199323cd7318d671893850dfe
thanhtung <75019711+thanhtung-2001@users.noreply.github.com>
2021-10-25 19:09:37 +0700

    customer

--- nhom_2/resources/views/admin/sidebar.blade.php
+++ nhom_2/resources/views/admin/sidebar.blade.php
@@ -22,22 +22,45 @@
 
 
         <li class="nav-item">
+<<<<<<< Updated upstream
             <a class="nav-link collapsed" data-bs-target="#components-nav2" data-bs-toggle="collapse" href="#">
                 <i class="bi bi-menu-button-wide"></i><span>Khách hàng</span><i class="bi bi-chevron-down ms-auto"></i>
+=======
+            <a class="nav-link collapsed" data-bs-target="#components-nav5" data-bs-toggle="collapse" href="#">
+
+                <i class="bi bi-menu-button-wide"></i><span> Bàn</span><i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="components-nav5" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="{{asset('table')}}">
+                        <i class="bi bi-circle"></i><span>Danh Sách Bàn</span>
+                    </a>
+            </ul>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#components-nav2" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-menu-button-wide"></i><span>Khách Hàng</span><i class="bi bi-chevron-down ms-auto"></i>
+>>>>>>> Stashed changes
             </a>
             <ul id="components-nav2" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                 <li>
+                    <a href="{{asset('customer')}}">
+
+                        <i class="bi bi-circle"></i><span>Danh Sách Khách Hàng</span>
+                    </a>
                     <a href="{{asset('customer/create')}}">
                         <i class="bi bi-circle"></i><span>Thêm Khách Hàng</span>
                     </a>
-
-                    <a href="{{asset('customer')}}">
-                        <i class="bi bi-circle"></i><span>Danh Sách Khách Hàng</span>
-                    </a>
+                </li>
             </ul>
+<<<<<<< Updated upstream
         </li><!-- End Components Nav -->
 
 
+=======
+        </li>
+>>>>>>> Stashed changes
     </ul>
 
 </aside><!-- End Sidebar-->
