79278d011503be4c31ff7f580ec685be2a6a87bb
your name <nguyenminhduc01032001@gmail.com>
2021-10-25 20:13:36 +0700

    table

---
+++ nhom_2/resources/views/table/add.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.main')
+@section('content')
+    <div @class('container')>
+        <p>Add new</p>
+
+        @if(count($errors) > 0)
+            <div @class('alert alert-danger')>
+                @foreach($errors->all() as $err)
+                    <p>{{$err}}</p>
+                @endforeach
+            </div>
+        @endif
+        <form  action="{{route('table.store')}}"method="post"
+               enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="title">type</label>
+                <input type="text" class="form-control"
+                       id="type" name="type">
+            </div>
+
+            <fieldset class="row mb-3">
+                <legend class="col-form-label col-sm-2 pt-0">Trạng Thái</legend>
+                <div class="col-sm-10">
+                    <div class="form-check">
+                        <input class="form-check-input" value="0" type="radio" name="status" id="gridRadios1" checked="">
+                        <label class="form-check-label" for="gridRadios1">
+                            Đang chờ
+                        </label>
+                    </div>
+
+
+                </div>
+            </fieldset>
+            <input type="submit" value="Save">
+        </form>
+    </div>
+
+@endsection
