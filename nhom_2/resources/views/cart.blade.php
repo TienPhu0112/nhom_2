25c197a99087dc3fca4fb1a6e7769134c0569901
ManhTran1511 <88219084+ManhTran1511@users.noreply.github.com>
2021-11-05 19:19:34 +0700

    Sửa cart

--- nhom_2/resources/views/cart.blade.php
+++ nhom_2/resources/views/cart.blade.php
@@ -1,5 +1,4 @@
 @extends("layout.frontend")
-
 @section('content')
     <section class="bg-title-page flex-c-m p-t-160 p-b-80 p-l-15 p-r-15" style="background-image: url(images/bg-title-page-01.jpg);">
         <h2 class="tit6 t-center">
