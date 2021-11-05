fa476d5ab47f96f7cf5baa36b8ad80b5f4bbdd49
ManhTran1511 <88219084+ManhTran1511@users.noreply.github.com>
2021-11-05 19:19:05 +0700

    Menu - Cart - Order - Checkout - Mail

--- nhom_2/resources/views/cart.blade.php
+++ nhom_2/resources/views/cart.blade.php
@@ -1,9 +1,5 @@
 @extends("layout.frontend")
-<!-- Core Style CSS -->
-<link rel="stylesheet" href="{{asset('cartfile/css/core-style.css')}}">
-<link rel="stylesheet" href="{{asset('cartfile/style.css')}}">
-<!-- Responsive CSS -->
-<link href="{{asset('cartfile/css/responsive.css')}}" rel="stylesheet">
+
 @section('content')
     <section class="bg-title-page flex-c-m p-t-160 p-b-80 p-l-15 p-r-15" style="background-image: url(images/bg-title-page-01.jpg);">
         <h2 class="tit6 t-center">
