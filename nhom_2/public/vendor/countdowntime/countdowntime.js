5b4e609c007e4f21524fd6e843c4258819e7cfe6
TienPhu0112 <74959126+TienPhu0112@users.noreply.github.com>
2021-11-11 03:58:46 +0700

    chinh sua dem thoi gian

--- nhom_2/public/vendor/countdowntime/countdowntime.js
+++ nhom_2/public/vendor/countdowntime/countdowntime.js
@@ -16,19 +16,22 @@
       };
     }
 
-    function initializeClock(id, endtime) {
-      var daysSpan = $('.days');
-      var hoursSpan = $('.hours');
-      var minutesSpan = $('.minutes');
-      var secondsSpan = $('.seconds');
+    function initializeClock(id, endtime, length) {
+      var daysSpan = document.getElementsByClassName('days');
+      var hoursSpan = document.getElementsByClassName('hours');
+      var minutesSpan = document.getElementsByClassName('minutes');
+      var secondsSpan = document.getElementsByClassName('seconds');
 
       function updateClock() {
-        var t = getTimeRemaining(endtime);
+        var t;
 
-        daysSpan.html(t.days);
-        hoursSpan.html(('0' + t.hours).slice(-2));
-        minutesSpan.html(('0' + t.minutes).slice(-2));
-        secondsSpan.html(('0' + t.seconds).slice(-2))
+        for (var i=0 ; i<length;i++){
+            t = getTimeRemaining(endtime[i]);
+            daysSpan[i].innerHTML = t.days;
+            hoursSpan[i].innerHTML = ('0' + t.hours).slice(-2);
+            minutesSpan[i].innerHTML = ('0' + t.minutes).slice(-2);
+            secondsSpan[i].innerHTML = ('0' + t.seconds).slice(-2);
+        }
 
         if (t.total <= 0) {
           clearInterval(timeinterval);
@@ -40,16 +43,28 @@
     }
 
     function getDate(){
-        var elm,length;
-        var date;
+        var elm,length,date;
+        var dateList = [];
         length = document.getElementsByClassName('get-date')[0].getAttribute('data-number');
-        elm = document.getElementsByClassName('get-date')[0];
-        date = elm.getAttribute('data-date');
-        return date;
+        for(var i=0; i < length;i++){
+            elm = document.getElementsByClassName('get-date')[i];
+            date = elm.getAttribute('data-date');
+            dateList.push(date);
+        }
+
+        return dateList;
     }
 
     console.log(getDate());
+    var length = document.getElementsByClassName('get-date')[0].getAttribute('data-number');
+    var deadline;
+    var deadlineList = [];
+    for (var i=0; i < length;i++){
+        deadline = new Date(Date.parse(getDate()[i]));
+        deadlineList.push(deadline);
+    }
 
-    var deadline = new Date(Date.parse(getDate()));
-    initializeClock('clockdiv', deadline);
+    console.log(deadlineList);
+
+    initializeClock('clockdiv', deadlineList,length);
 })(jQuery);
