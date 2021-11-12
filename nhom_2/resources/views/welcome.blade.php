6b72a20d3228de48989885e045ffcaf0ed2a6a0d
thanhtung <75019711+thanhtung-2001@users.noreply.github.com>
2021-11-12 18:37:41 +0700

    home

--- nhom_2/resources/views/welcome.blade.php
+++ nhom_2/resources/views/welcome.blade.php
@@ -161,21 +161,85 @@
             </div>
 
             <div class="row">
-                <div class="col-md-6">
-                    <!-- Item our menu -->
-                    @foreach($lsType as $type)
-                        <div class="item-ourmenu bo-rad-10 hov-img-zoom pos-relative m-t-30">
-                            @foreach($lsFood as $food)
-                                <img src="{{$food->image}}" alt="IMG-MENU">
-                            @endforeach
-                            <!-- Button2 -->
-                            <a href="{{asset('/menu')}}" class="btn2 flex-c-m txt5 ab-c-m size4">
-                                {{$type->name}}
-                            </a>
+                <div class="col-md-8">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <!-- Item our menu -->
+                            <div class="item-ourmenu bo-rad-10 hov-img-zoom pos-relative m-t-30">
+                                <img src="images/our-menu-01.jpg" alt="IMG-MENU">
+
+                                <!-- Button2 -->
+                                <a href="{{asset('/menu')}}" class="btn2 flex-c-m txt5 ab-c-m size4">
+                                    Lunch
+                                </a>
+                            </div>
                         </div>
-                    @endforeach
+
+                        <div class="col-sm-6">
+                            <!-- Item our menu -->
+                            <div class="item-ourmenu bo-rad-10 hov-img-zoom pos-relative m-t-30">
+                                <img src="images/our-menu-05.jpg" alt="IMG-MENU">
+
+                                <!-- Button2 -->
+                                <a href="{{asset('/menu')}}" class="btn2 flex-c-m txt5 ab-c-m size5">
+                                    Dinner
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <!-- Item our menu -->
+                            <div class="item-ourmenu bo-rad-10 hov-img-zoom pos-relative m-t-30">
+                                <img src="images/our-menu-13.jpg" alt="IMG-MENU">
+
+                                <!-- Button2 -->
+                                <a href="{{asset('/menu')}}" class="btn2 flex-c-m txt5 ab-c-m size6">
+                                    Happy Hour
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
+                <div class="col-md-4">
+                    <div class="row">
+                        <div class="col-12">
+                            <!-- Item our menu -->
+                            <div class="item-ourmenu bo-rad-10 hov-img-zoom pos-relative m-t-30">
+                                <img src="images/our-menu-08.jpg" alt="IMG-MENU">
+
+                                <!-- Button2 -->
+                                <a href="{{asset('/menu')}}" class="btn2 flex-c-m txt5 ab-c-m size7">
+                                    Drink
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <!-- Item our menu -->
+                            <div class="item-ourmenu bo-rad-10 hov-img-zoom pos-relative m-t-30">
+                                <img src="images/our-menu-10.jpg" alt="IMG-MENU">
+
+                                <!-- Button2 -->
+                                <a href="{{asset('/menu')}}" class="btn2 flex-c-m txt5 ab-c-m size8">
+                                    Starters
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <!-- Item our menu -->
+                            <div class="item-ourmenu bo-rad-10 hov-img-zoom pos-relative m-t-30">
+                                <img src="images/our-menu-16.jpg" alt="IMG-MENU">
+
+                                <!-- Button2 -->
+                                <a href="{{asset('/menu')}}" class="btn2 flex-c-m txt5 ab-c-m size9">
+                                    Dessert
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
